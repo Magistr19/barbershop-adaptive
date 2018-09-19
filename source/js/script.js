c4ea531c9324b58906b300902a1d15c8c6d96c0c
Alex <sashapayareli@gmail.com>
2018-09-19 22:30:37 +0300

    Оживил жаваскриптом мобильную навигацию и модальное окно логина

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,37 @@
-﻿
+/* Js active */
+
+var bodyElement = document.querySelector ('.no-js');
+bodyElement.classList.remove('no-js');
+
+/* Mobile nav open */
+
+var toogleBtn = document.querySelector ('.main-nav__toogle');
+var mobileNav = document.querySelector ('.main-nav__items-wrapper');
+
+toogleBtn.addEventListener("click", function(evt) {
+  if (toogleBtn.classList.contains('main-nav__toogle--open')) {
+    toogleBtn.classList.remove ('main-nav__toogle--open');
+    mobileNav.classList.remove ('main-nav__items-wrapper--open');
+  }
+  else {
+    toogleBtn.classList.add ('main-nav__toogle--open');
+    mobileNav.classList.add ('main-nav__items-wrapper--open');
+  }
+});
+
+/* Modal login */
+
+var loginLink = document.querySelector ('.main-nav__user-item--login a');
+var modalLogin = document.querySelector ('.modal-login');
+var closeBtn = document.querySelector ('.modal-login__close');
+
+loginLink.addEventListener("click", function(evt) {
+  toogleBtn.classList.remove ('main-nav__toogle--open');
+  mobileNav.classList.remove ('main-nav__items-wrapper--open');
+  modalLogin.classList.add ('modal-login--show');
+});
+
+closeBtn.addEventListener("click", function(evt) {
+  modalLogin.classList.remove ('modal-login--show');
+});
+
