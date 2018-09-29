441a51490f0d6f02593442234f08d12ccd3b8740
Alex <sashapayareli@gmail.com>
2018-09-29 04:20:27 +0300

    Настроил сборку для изображений

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var gulp = require("gulp");
 /* Подключение SASS препроцессора */
 var sass = require("gulp-sass");
 
-/* запирает все ошибки в себя, не останавливая работу скрипта */
+/* Запирает все ошибки в себя, не останавливая работу скрипта */
 var plumber = require("gulp-plumber");
 
 /* POSTCSS c автопрефикером */
@@ -19,6 +19,7 @@ var autoprefixer = require("autoprefixer");
 
 /* Модуль отображающий сайт в браузере */
 var server = require("browser-sync").create();
+var reload = server.reload;
 
 /* Минификация HTML */
 var htmlmin = require("gulp-htmlmin");
@@ -65,7 +66,7 @@ gulp.task("html", function() {       /* название таска*/
     ]))
     .pipe(htmlmin({                  /* Минификация HTML*/
       collapseWhitespace: true,
-      ignoreCustomFragments: [ /<br>\s/gi ]
+      ignoreCustomFragments: [ /<br>\s/gi ]  /*Не убираем пробел после <br> */
     }))
     .pipe(gulp.dest("./build"))      /* куда кидает файлы */
     .pipe(server.stream());          /* команда перезагрузки сервера в браузере */
@@ -80,7 +81,9 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("./build/css"))
-    .pipe(minify())
+    .pipe(minify({
+      restructure: false          /*Отключаем смешивание общих стилей, чтобы не страдать*/
+    }))
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("./build/css"))
     .pipe(server.stream());
@@ -104,12 +107,12 @@ gulp.task("images", function() {
       imagemin.jpegtran({progressive: true}),    /* прогрессивная загрузка jpg (сначала пиксельная, позже проявляется) */
       imagemin.svgo()   /*Минификация svg от лишних тегов*/
       ]))
-    .pipe(gulp.dest("./build/img"))
+    .pipe(gulp.dest("./build/img"));
 });
 
 /* Конвертация в webp*/
 gulp.task("webp", function() {
-  return gulp.src("./source/img/**/*.{png,jpg}")
+  return gulp.src("./build/img/towebp/**/*.{png,jpg}")
     .pipe(webp({quality: 90})) /* Конвертируем png/jpg в webp с легкой потерей качества изображения */
     .pipe(gulp.dest("./build/img"));
 });
@@ -124,33 +127,14 @@ gulp.task("sprite", function() {
       imagemin.svgo()
       ]))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("./build/img"))
-});
-
-/* Перед тем как таск serve стартует должен быть запущен style.
-gulp serve для запуска локального сервера */
-gulp.task("serve", function() {
-  server.init({           /* инициирует сервер */
-    server: "./build/",   /* говорим откуда смотреть сайт ( . - текущий каталог) */
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  /* Ватчеры следящие за изменениями наших файлов */
-  /* Препроцессорные ватчеры (следим за всеми Sass файлами во всех папках внутри папки Sass),
-   вторым аргументом передаем какой таск нужно запустить если один из файлов запустился */
-  gulp.watch("./source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("./source/*.html", ["html"]);
-  gulp.watch("./source/js/*.js", ["scripts"]);
+    .pipe(gulp.dest("./build/img"));
 });
 
 /* Таск для копирования */
 gulp.task("copy", function() {
   return gulp.src([
-    "./source/fonts/**/*.{woff,woff2}",
-    /*"./source/img/**"*/
+  "./source/fonts/**/*.{woff,woff2}"/*,
+  "./source/img/**"*/
   ], {
     base: "./source/"     /* Говорим что базовый путь начинается из корня */
   })
@@ -162,6 +146,21 @@ gulp.task("clean", function() {
   return del("build");
 });
 
+/* Удаление всех изображений*/
+gulp.task("clean-images", function() {
+  return del("./build/img/**/*.{png,jpg,svg,webp}");
+});
+
+/* Запуск всех тасков работы с изображениями*/
+gulp.task("images-watch", function() {
+  run(
+    "clean-images",
+    "images",
+    "webp",
+    "sprite",
+    "html"
+    );
+});
 
 /* Таск запуска npm run build */
 gulp.task("build", function(done) {
@@ -176,4 +175,24 @@ gulp.task("build", function(done) {
     "html",
     done  /* Самым последним вызовом функции run должна быть функция, которая была передана как аргумент */
   );
+});
+
+/* Перед тем как таск serve стартует должен быть запущен build.
+gulp serve для запуска локального сервера */
+gulp.task("serve", function() {
+  server.init({           /* инициирует сервер */
+    server: "./build/",   /* говорим откуда смотреть сайт ( . - текущий каталог) */
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  /* Ватчеры следящие за изменениями файлов */
+  /* Например, препроцессорные ватчеры (следим за всеми Sass файлами во всех папках внутри папки sass),
+   вторым аргументом передаем какой таск нужно запустить если один из файлов запустился */
+  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("source/*.html", ["html"]);
+  gulp.watch("source/js/*.js", ["scripts"]);
+  gulp.watch("source/img/**/*.{png,jpg,svg,webp}", ["images-watch"]);
 });
